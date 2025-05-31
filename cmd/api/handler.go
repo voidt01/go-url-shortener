a28a79389c301d6ce67904a225ebf6bbd6276fc5
Timothy Paendong <paendongtimothy7@gmail.com>
2025-05-31 15:21:42 +0700

    Refactor handler: improve request handling and validation for URL shortening. to do: make url request function to simpler shortening handler

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -3,43 +3,68 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/voidt01/go-url-shortener/internal/models"
 )
 
-func (a *App) Shortening(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
+// Request and Response for creating shortenURL
+type shortenRequest struct {
+	OriginalURL string `json:"original_url"`
+}
 
+type shortenResponse struct {
+	OriginalURL string `json:"original_url"`
+	ShortenURL  string `json:"shorten_url"`
+}
+
+func (a *App) Shortening(w http.ResponseWriter, r *http.Request) {
 	urlRequest := &shortenRequest{}
-	urlResponse := &shortenResponse{}
+	// urlResponse := &shortenResponse{}
 
 	// Decode POST Request body (JSON) to Go
-	err_decode := json.NewDecoder(r.Body).Decode(urlRequest)
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields() 
 
+	err_decode := dec.Decode(urlRequest)
 	if err_decode != nil {
 		a.clientError(w, http.StatusBadRequest)
 		return
 	}
 
+	// url nil value check
+	if urlRequest.OriginalURL == "" {
+		a.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// url validation
+	if !isValid(urlRequest.OriginalURL) {
+		a.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", urlRequest)
+
 	// Generate Short URL & Store urls in Database
-	shortCode, err_model := a.urls.Insert(urlRequest.OriginalURL)
-	if err_model != nil {
-		a.serveError(w, err_model)
-	}
+	// shortCode, err_model := a.urls.Insert(urlRequest.OriginalURL)
+	// if err_model != nil {
+	// 	a.serveError(w, err_model)
+	// }
 
-	// creating post response struct
-	urlResponse.OriginalURL = urlRequest.OriginalURL
-	urlResponse.ShortenURL = a.configApp.Server.baseURL + a.configApp.Server.port + "/" + shortCode
+	// // creating post response struct
+	// urlResponse.OriginalURL = urlRequest.OriginalURL
+	// urlResponse.ShortenURL = a.configApp.Server.baseURL + a.configApp.Server.port + "/" + shortCode
 
-	// encoding response struct (G0) to JSON
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	err_encode := json.NewEncoder(w).Encode(urlResponse)
+	// // encoding response struct (G0) to JSON
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusCreated)
+	// err_encode := json.NewEncoder(w).Encode(urlResponse)
 
-	if err_encode != nil {
-		a.errorLog.Printf("Error encoding response: %v", err_encode)
-	}
+	// if err_encode != nil {
+	// 	a.errorLog.Printf("Error encoding response: %v", err_encode)
+	// }
 
 }
 
@@ -59,12 +84,7 @@ func (a *App) Redirecting(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, value, http.StatusFound)
 }
 
-// Request and Response for creating shortenURL
-type shortenRequest struct {
-	OriginalURL string `json:"original_url"`
-}
-
-type shortenResponse struct {
-	OriginalURL string `json:"original_url"`
-	ShortenURL  string `json:"shorten_url"`
+func isValid(ori_url string) bool {
+	u, err := url.Parse(ori_url)
+	return err == nil && (u.Scheme == "https" || u.Scheme == "http")
 }
